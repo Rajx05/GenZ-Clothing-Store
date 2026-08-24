12be54ab3b41994f9b2540c92646c487fc63bf1a
Rajeev Negi <rajeevnegi005@gmail.com>
2026-08-24 19:30:33 +0530

    fixed duplicate key error when seller adds more than 1 product

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import config from "./config.js";
+import Product from "../models/product.model.js";
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(config.MONGO_URI);
+    await Product.syncIndexes();
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Database Connection Error: ${error.message}`);
